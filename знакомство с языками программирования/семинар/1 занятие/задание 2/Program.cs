1e921aa19213d43e810d191b45f446086b58d365
Никита <nikita@MacBook-Pro-Nikita.local>
2022-10-11 11:52:07 +0300

    задание 2

---
+++ знакомство с языками программирования/семинар/1 занятие/задание 2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число возведенное в квадрат");
+int userNumberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число, которое возводится в квадрат");
+int userNumberB = Convert.ToInt32(Console.ReadLine());
+int square = userNumberB * userNumberB;
+if(square == userNumberA)
+{
+Console.WriteLine("Да");
+}
+else
+{
+   Console.WriteLine("Нет") ;
+}
