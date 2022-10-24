c0e487888643ed1fd0f5a925321a22ba1b3a6973
Никита <nikita@MacBook-Pro-Nikita.local>
2022-10-24 10:13:02 +0300

    написали длинную строку по нахождению большей цифры из числа

---
+++ знакомство с языками программирования/семинар/24.10/Program.cs
@@ -0,0 +1,27 @@
+﻿int getRandomNumberFromRange(int minBorder, int maxBorder)
+{
+    int result = new int();
+    result = new Random().Next(minBorder, maxBorder + 1);
+    return result;
+}
+int getMaxDigitFromNumber(int number)
+{
+    int maxDigit = 0;
+    while(number > 0)
+    {
+        int currentDigit = number % 10;
+        if(maxDigit < currentDigit)
+        {
+            maxDigit = currentDigit;
+        }
+        number = number / 10;
+    }
+    return maxDigit;
+
+}
+
+    int RandomNumber = getRandomNumberFromRange(100,999);
+    Console.WriteLine($"Случайное значение {RandomNumber}");
+    int maxDigit = getMaxDigitFromNumber(RandomNumber);
+    Console.WriteLine($"Для числа {RandomNumber} большая цифра {maxDigit}");
+
